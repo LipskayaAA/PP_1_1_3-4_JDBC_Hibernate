d63b11eeffa87fb3bea5d7f4cadae31c8cb579e5
buloc <lipskaya2001@bk.ru>
2023-07-01 17:03:42 +0300

    n

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Driver;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+//    private final String URL = "jdbc:mysql://localhost:3306/db_new";
+//    private final String USERNAME = "root";
+//    private final String PASSWORD = "lipa89045902465";
+
+    public static Connection getConnection() {
+
+         final String URL = "jdbc:mysql://localhost:3306/db_new";
+         final String USERNAME = "root";
+         final String PASSWORD = "lipa89045902465";
+        Connection connection = null;
+
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection OK!");
+
+        } catch (SQLException e) {
+            System.out.println("Connection ERROR!");
+        }
+        return connection;
+    }
 }
